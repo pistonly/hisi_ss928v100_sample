6578b85c74994233f9f943d24bbfee2ad8011cda
liuyang <liuyang08035@gmail.com>
2024-05-22 15:35:09 +0800

    add haisi-sample_ive

---
+++ get_link_lib.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+with open("out.log") as f:
+    lines = f.readlines()
+
+link_list = []
+link_dirs = []
+link_dict = {}
+for l in lines:
+    l_list = l.split()
+    link_files = [Path(l_i) for l_i in l_list if l_i.endswith(".a")]
+    for p in link_files:
+        k = str(p.parent)
+        if k not in link_dict:
+            link_dict[k] = set()
+        link_dict[k].add(p.name)
+print(link_dict)
+    
+
+
